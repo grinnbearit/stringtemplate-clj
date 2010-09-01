9b2156dbf76ad0436317f27e6c28e4788d0a1c72
Sidhant Godiwala <sidhant.godiwala@infinitelybeta.com>
2010-09-01 13:28:52 +0530

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject stringtemplate-clj "0.1.0"
+  :description "A stringtemplate wrapper in clojure"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.antlr/stringtemplate "3.2"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]
+                     [lein-clojars "0.5.0"]])
---
+++ test/stringtemplate_clj/test/core.clj
@@ -0,0 +1,6 @@
+(ns stringtemplate-clj.test.core
+  (:use [stringtemplate-clj.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
